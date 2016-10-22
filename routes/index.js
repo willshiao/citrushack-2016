82666d2f2026112cbff9b71cb5ec28c90b0bd52d
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2016-10-22 09:14:30 -0700

    Fixed modal

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('pages/index.hbs', { title: 'Express', siteName: 'Collaborooms' });
+  res.render('pages/interface.hbs', { title: 'Collaborooms', siteName: 'Collaborooms' });
 });
 
 router.get('/login', (req, res, next) => {
