33e5cb0b15a549858e5a48939e6ca72f665a06b6
William Shiao <willshiao@gmail.com>
2016-10-22 23:35:42 -0700

    feat: make tasks a part of the User model

--- models/Room.js
+++ models/Room.js
@@ -5,9 +5,23 @@ const Schema = mongoose.Schema;
 const config = require('config');
 const shortid = require('shortid');
 
+const taskSchema = new Schema({
+  name: String,
+  slug: { type: String, default: shortid.generate },
+  isChecklist: Boolean,
+  listItems: [{
+    text: String,
+    completed: Boolean,
+  }],
+  description: String,
+  deleted: {type: Boolean, default: false},
+  roomSlug: String,
+});
+
 const roomSchema = new Schema({
   name: String,
   slug: { type: String, default: shortid.generate },
+  tasks: [taskSchema],
 });
 
 module.exports = mongoose.model('Room', roomSchema);
--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,23 @@ socket.on('connect', function() {
 });
 
 $(function() {
+  $('#sortable').sortable();
+  $('#sortable').disableSelection();
+
+  socket.emit('task:get');
+
+  $('#checklist-checkbox').change(function() {
+    if(this.checked) {
+      $('#description-container').hide();
+    } else {
+      $('#description-container').show();
+    }
+  });
+
   $('#new-task-form').submit(function(evt) {
     evt.preventDefault();
     socket.emit('task:new', {
-      title: $('#task-name-input').val(),
+      name: $('#task-name-input').val(),
     });
   });
 
@@ -42,3 +55,49 @@ socket.on('room:join:res', function(data) {
   $('#room').text(data.name);
   $('#roomId').text('(' + data.roomId + ')');
 });
+
+socket.on('task:new:res', data => {
+  if(!data.success) return console.error(data.message);
+  console.log('Added new task successfully');
+});
+
+socket.on('task:get:res', function(tasks) {
+  console.log('Got tasks:', tasks);
+  clearTasks();
+  var taskElements = tasks
+    .map(task => makeTaskElement(task.name, task.content));
+  for(var i = 0; i < taskElements.length; ++i) {
+    $('#tasks').append(taskElements[i]);
+  }
+});
+
+socket.on('update:task:new', function(task) {
+  console.log('Got new task:', task);
+  $('#tasks').append(makeTaskElement(task.name, task.content));
+});
+
+function clearTasks() {
+  $('#tasks').html('');
+}
+
+function makeTaskElement(title, content) {
+  return '<div class="card card-block tasks">\n' +
+    '<h4 class="card-title task-title">' + escapeHtml(title) + '</h4>\n' +
+    '<p class="task-content">' + escapeHtml(content || '') + '</p>\n' +
+    '</div>';
+}
+
+var entityMap = {
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  '"': '&quot;',
+  "'": '&#39;',
+  "/": '&#x2F;'
+};
+
+function escapeHtml(string) {
+  return String(string).replace(/[&<>"'\/]/g, function (s) {
+    return entityMap[s];
+  });
+}
